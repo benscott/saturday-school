a7aa88f6f12dee43b71f87a12ba22a22ea2375ef
benscott.co.uk <ben@benscott.co.uk>
2019-05-06 17:21:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+https://medium.com/front-end-weekly/what-are-npm-yarn-babel-and-webpack-and-how-to-properly-use-them-d835a758f987
+
+yarn run dev  
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+
+module.exports = {
+    optimization: {
+        minimizer: [new OptimizeCSSAssetsPlugin({})],
+    },
+    entry: {
+        main: [
+            // './assets/js/main.js', 
+            './src/sass/main.scss'
+        ],
+    },
+    output: {
+        filename: "[name].min.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'images'
+                    },
+                },
+            },
+            {
+                test: /\.(html)$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(css|scss)/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].min.css',
+        }),
+        new HtmlWebpackPlugin({
+            hash: true,
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "../dist/"),
+        port: 9000,
+        hot: true,
+        inline: true,
+    }
+};
